c1c49b14e9e565335ee22d25148aabc30b3c4b93
HerranzD <herranz@ifca.unican.es>
2022-03-10 17:59:09 +0100

    Adding a config.py file for defining the python path

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 16:59:44 2022
+
+@author: herranz
+"""
+
+import sys
+sys.path.append('/Users/herranz/Dropbox/Trabajo/Programs/Python')
