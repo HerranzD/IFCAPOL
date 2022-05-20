f58d9dc5d4bfc1e3f7f62aa9ef37bd6a1cbe472d
HerranzD <herranz@ifca.unican.es>
2022-05-20 16:37:38 +0200

    Adding an automated script generator script_generate.py

--- run_IFCAPOL.py
+++ run_IFCAPOL.py
@@ -9,13 +9,18 @@ Created on Fri May 20 13:40:47 2022
 import sys
 import PTEP_simulations as PTEP
 
-chan_number = int(sys.argv[0])
-sim_number  = int(sys.argv[1])
+args        = sys.argv[1]
+L           = len(args)
+p1          = L-2
+p2          = L-1
+
+chan_number = int(sys.argv[p1])
+sim_number  = int(sys.argv[p2])
 
 nmax        = len(PTEP.LB_channels)
 
-if chan_number <= nmax:
-    chan_name = PTEP.LB_channels[chan_number-1]
+if chan_number < nmax:
+    chan_name = PTEP.LB_channels[chan_number]
     if sim_number < 100:
         PTEP.detect_sources(sim_number,chan_name)
     else:
---
+++ script_generate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 16:20:39 2022
+
+@author: herranz
+"""
+
+from myutils import save_ascii_list
+import PTEP_simulations as PTEP
+
+nchans = len(PTEP.survey.LB_channels)
+nsims  = 100
+
+command_list = []
+
+for ichan in range(nchans):
+    for isim in range(nsims):
+
+        lsta = []
+        lsta.append('#!/bin/bash')
+        lsta.append('#SBATCH -N 1')
+        lsta.append('#SBATCH -C haswell')
+        lsta.append('#SBATCH -q regular')
+        lsta.append('#SBATCH -J IFCAPOL_{0}_{1}'.format(ichan,isim))
+        lsta.append('#SBATCH --mail-user=herranz@ifca.unican.es')
+        lsta.append('#SBATCH --mail-type=ALL')
+        lsta.append('#SBATCH --account=mp107')
+        lsta.append('#SBATCH -t 01:30:00')
+        lsta.append('#SBATCH --output={0}Output_Logs/IFCAPOL_nchan{1}_nsim{2}.out'.format(PTEP.survey.scriptd,ichan,isim))
+        lsta.append('#SBATCH --chdir={0}'.format(PTEP.survey.scriptd))
+        lsta.append(' ')
+        lsta.append('#run the application:')
+        lsta.append('module load python')
+        lsta.append('source activate pycmb')
+        lsta.append('srun -n 1 -c 1 python3 $HOME/LiteBIRD/src/run_IFCAPOL.py {0} {1}'.format(ichan,isim))
+        lsta.append('conda deactivate')
+
+        macro_name = PTEP.survey.scriptd+'submit_nchan{0}_nsim{1}.sh'.format(ichan,isim)
+        save_ascii_list(lsta,macro_name)
+        command_list.append('sbatch {0}'.format(macro_name))
+
+fname = PTEP.survey.scriptd+'send.sh'
+save_ascii_list(fname)
+
+
+
+
