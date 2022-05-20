940041854fbd3f73a72861a59b35f173d8941608
HerranzD <herranz@ifca.unican.es>
2022-05-20 13:15:56 +0200

    Adding an executable for creating the input point source catalogues from the PTEP simulations

---
+++ run_create_source_catalogues.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 13:13:29 2022
+
+@author: herranz
+"""
+
+import PTEP_simulations as PTEP
+
+PTEP.create_point_source_catalogues()
