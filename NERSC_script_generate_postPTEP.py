5af84904e3b7bea4c731457a566baa48f6ad488f
HerranzD <herranz@ifca.unican.es>
2023-10-06 10:08:32 +0200

    Restoring the base script generation script in NERSC_script_generate_postPTEP.py

--- NERSC_script_generate_postPTEP.py
+++ NERSC_script_generate_postPTEP.py
@@ -1,26 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-This script generates Slurm scripts needed to run the IFCAPOL source extraction over all the LiteBIRD postPTEP Run0 simulations at NERSC. It also generates shell scripts needed to submit the IFCAPOL source extraction over a list of LiteBIRD postPTEP Run0 simulations at NERSC. The script checks the status of the submissions for a given channel and generates a new submission script for the simulations that do not have an output log file and do not have both output catalogues. 
+Created on Fri May 20 16:20:39 2022
 
-Functions:
-----------
-make_script(isim,ichan,hours=1,minutes=30)
-    Generates a Slurm script needed to run the IFCAPOL source extraction over one of the LiteBIRD postPTEP Run0 simulations at NERSC.
-    
-generate_slurm_scripts()
-    Generates the Slurm scripts needed to run the IFCAPOL source extraction over all the LiteBIRD postPTEP Run0 simulations at NERSC.
-    
-generate_submission_script(ichan,sim_list=None)
-    Generates a shell script needed to submit the IFCAPOL source extraction over a list of LiteBIRD postPTEP Run0 simulations at NERSC.
-    
-test_generate_submission_script()
-    Tests the generation of a submission script for a single channel and a given list of simulations.
-    
-generate_all_submission_scripts()
-    Generates the shell scripts needed to submit the IFCAPOL source extraction over all the LiteBIRD postPTEP Run0 simulations at NERSC.
-    
-check_submission_status(ichan)
-    Checks the status of the submissions for a given channel and generates a new submission script for the simulations that do not have an output log file and do not have both output catalogues.
+@author: herranz
 """
+
+from myutils import save_ascii_list
+import run0_simulations as postPTEP
+import os
+import sys
+
+nchans = len(postPTEP.LB_channels)
+nsims  = 200
+Njobs  = nsims
+
+# %% --- SLURM SCRIPT GENERATION
+
+def make_script(isim,ichan,hours=1,minutes=30):
+    """
+    This routine generates automatically a Slurm script needed to run
+    the IFCAPOL source extraction over one of the LiteBIRD postPTEP Run0
+    simulations at NERSC.
+
+    Parameters
+    ----------
+    isim : int
+        The simulation number.
+    ichan : int
+        The channel number.
+    hours : int, optional
+        The number of hours to run the simulation for. Default is 1.
+    minutes : int, optional
+        The number of minutes to run the simulation for. Default is 30.
+
+    Returns
+    -------
+    None.
+
+    """
+    time_str = '0{0}:{1}:00'.format(hours,minutes)
+
+    lsta = []
+    lsta.append('#!/bin/bash')
+    lsta.append('#SBATCH -N 1')
+    lsta.append('#SBATCH -C haswell')
+    lsta.append('#SBATCH -q regular')
+    lsta.append('#SBATCH -J IFCAPOL_{0}_{1}'.format(ichan,isim))
+    lsta.append('#SBATCH --mail-user=herranz@ifca.unican.es')
+    lsta.append('#SBATCH --mail-type=ALL')
+    lsta.append('#SBATCH --account=mp107')
+    lsta.append('#SBATCH -t {0}'.format(time_str))
+    lsta.append('#SBATCH --output={0}Output_Logs/IFCAPOL_nchan{1}_nsim{2}.out'.format(postPTEP.survey.scriptd,ichan,isim))
+    lsta.append('#SBATCH --chdir={0}'.format(postPTEP.survey.scriptd))
+    lsta.append(' ')
+    lsta.append('#run the application:')
+    lsta.append('module load python')
+    lsta.append('source activate pycmb')
+    lsta.append('srun -n 1 -c 1 -t {0} python3 $HOME/LiteBIRD/src/run_IFCAPOL_on_postPTEP.py {1} {2}'.format(time_str,ichan,isim))
+    lsta.append('conda deactivate')
+
+    macro_name = postPTEP.survey.scriptd+'run_nchan{0}_nsim{1}.slurm'.format(ichan,isim)
+    save_ascii_list(lsta,macro_name)
+
+def generate_slurm_scripts():
+    """
+    This routine generates automatically the Slurm scripts needed to run
+    the IFCAPOL source extraction over all the LiteBIRD postPTEP Run0
+    simulations at NERSC. The number of hours to run the simulations increases
+    by one hour for each five simulations, starting with 1 for simulation 0.
+
+    Returns
+    -------
+    None.
+
+    """
+    for ichan in range(nchans):
+        for isim in range(nsims):
+            hours   = 1 + isim//5
+            minutes = 30    
+            make_script(isim,ichan,hours,minutes)
+
+# %% --- SUBMISSION SCRIPTS
+
+def generate_submission_script(ichan,sim_list=None):
+    """
+    This routine generates automatically a shell script needed to submit
+    the IFCAPOL source extraction over a list of LiteBIRD postPTEP Run0
+    simulations at NERSC. The list refer to a set of simulations for a
+    given channel.
+
+    Parameters
+    ----------
+    ichan : int
+        The channel number. Can take values from 0 to the number of 
+        LiteBIRD channels (22) minus one.
+    sim_list : list, optional
+        The list of simulation numbers to run. If None, all the simulations
+        for the given channel are run. The default is None. The list can be
+        a subset of the simulations for the given channel.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    if sim_list is not None:
+        lista = sim_list.copy()
+    else:
+        lista = [i for i in range(nsims)]
+
+    fname = postPTEP.survey.scriptd+'submit_nchan{0}.sh'.format(ichan)
+
+    lsubmits = []
+
+    for isim in lista:
+        macro_name = postPTEP.survey.scriptd+'run_nchan{0}_nsim{1}.slurm'.format(ichan,
+                                                                                 isim)
+        lsubmits.append('sbatch '+macro_name)
+
+    save_ascii_list(lsubmits,fname)
 
 # Test the routine with a single simulation for channel 0 and sim_list = [0,1,2]
 
@@ -138,15 +238,14 @@ def check_submission_status(ichan):
 
 if __name__ == '__main__':
     """
-    This routine runs the routines from the command line. The argument is
-    given when the script is called from the command line. The argument can
-    be 'generate_slurm_scripts', 'generate_all_submission_scripts',
-    'check_submission_status' or 'test_generate_submission_script'.
-
-    Returns
-    -------
-    None.
-
+    This routine runs the routines in this file from the command line. It 
+    can take  four arguments:
+        generate_slurm_scripts
+        generate_all_submission_scripts
+        check_submission_status
+        test_generate_submission_script
+    The first three arguments run the routines with the same name. The last
+    argument runs the routine test_generate_submission_script().
     """
 
     if len(sys.argv) > 1:
